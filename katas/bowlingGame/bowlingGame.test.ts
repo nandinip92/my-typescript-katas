9c3bc78efa0d956d1b6ab56fbf77b74e178d1061
nandinip92 <nandini.p8892@gmail.com>
2023-08-10 09:07:11 +0100

    tests: Adding new testcases

--- katas/bowlingGame/bowlingGame.test.ts
+++ katas/bowlingGame/bowlingGame.test.ts
@@ -2,13 +2,34 @@ import { bowlingGame } from "./bowlingGame";
 
 describe("bowlingGame", () => {
   test("Returns the score of the game", () => {
+    //Arrange
     let rolls = "X X X X X X X X X X X X";
+    //Action and Assert
     expect(bowlingGame(rolls)).toBe(300);
 
+    //Arrange
     rolls = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-";
+    //Action and Assert
     expect(bowlingGame(rolls)).toBe(90);
 
+    //Arrange
     rolls = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5";
+    //Action and Assert
     expect(bowlingGame(rolls)).toBe(150);
+
+    //Arrange
+    rolls = "15 36 72 36 44 53 33 45 81 26";
+    //Action and Assert
+    expect(bowlingGame(rolls)).toBe(81);
+
+    //Arrange
+    rolls = "X 36 72 36 44 53 33 45 81 26";
+    //Action and Assert
+    expect(bowlingGame(rolls)).toBe(94);
+
+    //Arrange
+    rolls = "1/ 36 72 36 44 53 33 45 81 26";
+    //Action and Assert
+    expect(bowlingGame(rolls)).toBe(88);
   });
 });
